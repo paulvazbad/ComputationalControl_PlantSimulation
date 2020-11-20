109c90ad0e520d2fc6ccb4b9771be5d85b8601e0
Paul Vazquez <pev@live.com.mx>
2020-11-19 23:40:09 -0800

    Added test cases in main.py

--- src/main.py
+++ src/main.py
@@ -25,15 +25,19 @@ def main():
     #primer_orden(0.5, 4, 2, 1, escalon)
     #ARX_filter(4, [2, 3, 4, 5], b_values, 0, 0, entrada_test)
     plt.axis([0, 60, 0, 100])
-
+    x = []
+    y_plot = []
 
     if(isinstance(input_to_the_system, list)):
         time_to_plot = len(input_to_the_system)
 
     while(seconds < time_to_plot):
-        y = primer_orden(0.5, 4, 2, seconds, 1, input_to_the_system)
+        x.append(seconds)
+        y = primer_orden(0.5, 3.34, 1, seconds, 1.46, input_to_the_system,0)
+        y_plot.append(y[0])
         print("Value of primer orden at " + str(seconds) + " is " + str(y))
-        plt.scatter(seconds, y + disturbance)
+       
+        plt.scatter(x, y_plot)
         plt.pause(0.5)
         seconds += 1
     plt.show()
